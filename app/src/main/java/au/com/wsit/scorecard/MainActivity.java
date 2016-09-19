fc7ac41db12b04382e84161e6b49476b0d488b13
Guy Bridge <guy@wsit.com.au>
2016-09-20 07:43:17 +0800

    Added menu layout

--- app/src/main/java/au/com/wsit/scorecard/MainActivity.java
+++ app/src/main/java/au/com/wsit/scorecard/MainActivity.java
@@ -2,6 +2,8 @@ package au.com.wsit.scorecard;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +13,29 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    // Setup the menu options
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    // On menu item selected method
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        int id = item.getItemId();
+
+        switch (id)
+        {
+            case R.id.action_clear_strokes:
+                // Clear strokes
+                break;
+        }
+        
+        return super.onOptionsItemSelected(item);
     }
 }
